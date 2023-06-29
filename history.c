da1e56ad760f95656e1c447b14d4c411b3b96dc2
Euryna <majdouline.bastou@gmail.com>
2023-06-28 17:30:17 -0700

    simple shell

--- history.c
+++ history.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 
 /**
- * get_history_file - shows the history of the file
- * @info: parameter's structure
- * Return: string containg history file
+ * get_history_file - gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
  */
 
 char *get_history_file(info_t *info)
@@ -24,9 +25,10 @@ char *get_history_file(info_t *info)
 }
 
 /**
- * write_history - writes history of file
- * @info: the parameter's structure
- * Return: 1 on success, otherwise -1
+ * write_history - creates a file, or appends to an existing file
+ * @info: the parameter struct
+ *
+ * Return: 1 on success, else -1
  */
 int write_history(info_t *info)
 {
@@ -52,9 +54,10 @@ int write_history(info_t *info)
 }
 
 /**
- * read_history - reads history
- * @info: the parameter's structure
- * Return: historycount on success, 0 otherwise
+ * read_history - reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount on success, 0 otherwise
  */
 int read_history(info_t *info)
 {
@@ -92,18 +95,19 @@ int read_history(info_t *info)
 	if (last != i)
 		build_history_list(info, buf + last, linecount++);
 	free(buf);
-	info->historycount = linecount;
-	while (info->historycount-- >= HIST_MAX)
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
 		delete_node_at_index(&(info->history), 0);
 	renumber_history(info);
-	return (info->historycount);
+	return (info->histcount);
 }
 
 /**
- * build_history_list - builds a history linked list
- * @info: The parameter's structure
+ * build_history_list - adds entry to a history linked list
+ * @info: Structure containing potential arguments. Used to maintain
  * @buf: buffer
- * @linecount: history linecount
+ * @linecount: the history linecount, histcount
+ *
  * Return: Always 0
  */
 int build_history_list(info_t *info, char *buf, int linecount)
@@ -120,9 +124,10 @@ int build_history_list(info_t *info, char *buf, int linecount)
 }
 
 /**
- * renumber_history - renumbers the history
- * @info: The parameter's structure
- * Return: History count
+ * renumber_history - renumbers the history linked list after changes
+ * @info: Structure containing potential arguments. Used to maintain
+ *
+ * Return: the new histcount
  */
 int renumber_history(info_t *info)
 {
